83e1f62a18f1e262aecdf5491b7a4020642f26f8
fauzani96 <muhammadfauzanisa@gmail.com>
2022-05-25 01:41:35 +0700

    loads font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,11 +13,6 @@ export default class MyDocument extends Document {
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
 
-          <link
-            rel="stylesheet"
-            href="http://fonts.cdnfonts.com/css/gt-walsheim"
-          />
-
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
         </Head>
