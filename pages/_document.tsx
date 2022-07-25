ca083ca7dbede59476e009351a78c36b5abd074d
fauzani96 <muhammadfauzanisa@gmail.com>
2022-07-25 22:11:52 +0700

    fix font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,10 +16,6 @@ export default class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
-          <link
-            href="http://fonts.cdnfonts.com/css/renogare"
-            rel="stylesheet"
-          />
           {/* Inject MUI styles first to match with the prepend: true configuration. */}
           {(this.props as any).emotionStyleTags}
         </Head>
